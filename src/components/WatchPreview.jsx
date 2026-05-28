4cd8fd8ed2c65fc3466d8b39c393d854ee12ff9d
CJK-code04 <camiel.kneefel@gmail.com>
2026-05-28 16:36:11 +0200

    Tune WatchPreview strap and bezel composition

--- src/components/WatchPreview.jsx
+++ src/components/WatchPreview.jsx
@@ -12,82 +12,77 @@ function assetSrc(part, folder) {
   return `${baseUrl}assets/${folder}/${part.id}.png`;
 }
 
-const dimensions = {
-  normal: {
-    stage: 320,
-    strap: 790,
-    case: 210,
-    dial: 158,
-    bezel: 210
+const stageStyle = {
+  position: "relative",
+  width: "320px",
+  height: "320px",
+  maxWidth: "100%",
+  margin: "0 auto"
+};
+
+const smallStageStyle = {
+  ...stageStyle,
+  width: "220px",
+  height: "220px"
+};
+
+const baseLayerStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  objectFit: "contain",
+  pointerEvents: "none",
+  userSelect: "none"
+};
+
+const layerStyle = ({ size, zIndex, x = 0, y = 0 }) => ({
+  ...baseLayerStyle,
+  width: size,
+  height: size,
+  zIndex,
+  transform: `translate(calc(-50% + ${x}px), calc(-50% + ${y}px))`
+});
+
+const layerSizes = {
+  regular: {
+    strap: { rubber: "790px", fabric: "790px", leather: "990px", steel: "1065px", zIndex: 1 },
+    case: { size: "210px", zIndex: 2 },
+    dial: { size: "166px", zIndex: 3 },
+    bezel: { size: "228px", zIndex: 4 }
   },
   small: {
-    stage: 220,
-    strap: 545,
-    case: 145,
-    dial: 109,
-    bezel: 145
+    strap: { rubber: "545px", fabric: "545px", leather: "680px", steel: "735px", zIndex: 1 },
+    case: { size: "145px", zIndex: 2 },
+    dial: { size: "115px", zIndex: 3 },
+    bezel: { size: "158px", zIndex: 4 }
   }
 };
 
-const offsets = {
-  normal: {
-    strap: { x: 0, y: 0 },
-    case: { x: 0, y: 0 },
-    dial: { x: -4, y: -3 },
-    bezel: { x: -4, y: -3 }
-  },
-  small: {
-    strap: { x: 0, y: 0 },
-    case: { x: 0, y: 0 },
-    dial: { x: -3, y: -2 },
-    bezel: { x: -3, y: -2 }
-  }
-};
-
-function stageStyle(size) {
-  return {
-    position: "relative",
-    width: `${size.stage}px`,
-    height: `${size.stage}px`,
-    maxWidth: "88vw",
-    maxHeight: "88vw",
-    margin: "0 auto"
-  };
-}
-
-function layerStyle(size, offsetSet, layer, zIndex) {
-  const offset = offsetSet[layer] || { x: 0, y: 0 };
-
-  return {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    width: `${size[layer]}px`,
-    height: `${size[layer]}px`,
-    objectFit: "contain",
-    transform: `translate(calc(-50% + ${offset.x}px), calc(-50% + ${offset.y}px))`,
-    pointerEvents: "none",
-    userSelect: "none",
-    zIndex
-  };
+function strapSizeFor(strap, sizes) {
+  if (strap.material === "Staal") return sizes.strap.steel;
+  if (strap.material === "Leer") return sizes.strap.leather;
+  if (strap.material === "Stoffen") return sizes.strap.fabric;
+  return sizes.strap.rubber;
 }
 
 export default function WatchPreview({ combo, small = false }) {
-  const mode = small ? "small" : "normal";
-  const size = dimensions[mode];
-  const offsetSet = offsets[mode];
   const strapSrc = assetSrc(combo.strap, "straps");
   const caseSrc = assetSrc(combo.watchCase, "cases");
   const dialSrc = assetSrc(combo.moon, "dials");
   const bezelSrc = assetSrc(combo.bezel, "bezels");
+  const sizes = small ? layerSizes.small : layerSizes.regular;
+  const strapStyle = {
+    size: strapSizeFor(combo.strap, sizes),
+    zIndex: sizes.strap.zIndex
+  };
 
   return (
     <div className={`watch-preview ${small ? "watch-preview-small" : ""} ${backgroundClass(combo)}`}>
-      <div className="watch-stack" style={stageStyle(size)} aria-label={`${combo.name} preview`}>
-        <img src={strapSrc} alt={`${combo.strap.name} laag`} draggable="false" style={layerStyle(size, offsetSet, "strap", 1)} />
-        <img src={caseSrc} alt={`${combo.watchCase.name} laag`} draggable="false" style={layerStyle(size, offsetSet, "case", 2)} />
-        <img src={dialSrc} alt={`${combo.moon.name} wijzerplaat`} draggable="false" style={layerStyle(size, offsetSet, "dial", 3)} />
-        <img src={bezelSrc} alt={`${combo.bezel.name} laag`} draggable="false" style={layerStyle(size, offsetSet, "bezel", 4)} />
+      <div className="watch-stack" style={small ? smallStageStyle : stageStyle} aria-label={`${combo.name} preview`}>
+        <img src={strapSrc} alt={`${combo.strap.name} laag`} draggable="false" style={layerStyle(strapStyle)} />
+        <img src={caseSrc} alt={`${combo.watchCase.name} laag`} draggable="false" style={layerStyle(sizes.case)} />
+        <img src={dialSrc} alt={`${combo.moon.name} wijzerplaat`} draggable="false" style={layerStyle(sizes.dial)} />
+        <img src={bezelSrc} alt={`${combo.bezel.name} laag`} draggable="false" style={layerStyle(sizes.bezel)} />
       </div>
     </div>
   );
