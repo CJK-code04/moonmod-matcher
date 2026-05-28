f2212de8b2d1cb8dcb5f0ad50ae8379f6c698f43
CJK-code04 <camiel.kneefel@gmail.com>
2026-05-28 11:10:39 +0200

    Add case image paths

--- src/data/cases.js
+++ src/data/cases.js
@@ -1,4 +1,4 @@
 export const cases = [
-  { id: "case_steel", name: "Stalen kast", material: "Staal", color: "Staal", colorFamily: "Neutraal", tags: ["zakelijk", "formeel", "daily", "clean"], className: "case-steel" },
-  { id: "case_black", name: "Zwarte kast", material: "Zwart", color: "Zwart", colorFamily: "Neutraal", tags: ["sportief", "strak", "modern"], className: "case-black" }
+  { id: "case_steel", name: "Stalen kast", material: "Staal", color: "Staal", colorFamily: "Neutraal", image: "/assets/cases/case_steel.png", tags: ["zakelijk", "formeel", "daily", "clean"], className: "case-steel" },
+  { id: "case_black", name: "Zwarte kast", material: "Zwart", color: "Zwart", colorFamily: "Neutraal", image: "/assets/cases/case_black.png", tags: ["sportief", "strak", "modern"], className: "case-black" }
 ];
